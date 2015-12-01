2464304941f719603d8e870b1e085ab694780ceb
xinfeng128 <150389523@qq.com>
2015-12-01 13:25:19 +0800

    Initial Commit

---
+++ TestTools/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestTools
+//
+//  Created by wanglong on 15/12/1.
+//  Copyright © 2015年 wanglong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
